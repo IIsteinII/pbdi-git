895d91a28a1a28dd194c002ee047edef6a9d9889
Artur Stein <arturstein2004@gmail.com>
2024-02-27 09:00:25 -0300

    test(git-python): funcao subtrai teste

--- teste_calculadora.py
+++ teste_calculadora.py
@@ -5,5 +5,8 @@ def main():
     b = 3
     soma = calculadora.somar(a,b)
     print(f'{a} + {b} = {soma}')
+    subtracao = calculadora.subtrair(a,b)
+    print(f'{a} - {b} = {subtracao}')
+
 
 main()
